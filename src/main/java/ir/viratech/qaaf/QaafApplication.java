7c2e83811be0fa259f2fac4b191ef8e7d3dea985
yadegari <yadegari@viratech.ir>
2019-11-03 09:25:29 +0330

    initial commit

---
+++ src/main/java/ir/viratech/qaaf/QaafApplication.java
@@ -0,0 +1,14 @@
+package ir.viratech.qaaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QaafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QaafApplication.class, args);
+	}
+	
+}
+
---
+++ src/test/java/ir/viratech/qaaf/AppTest.java
@@ -0,0 +1,12 @@
+package ir.viratech.qaaf;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class AppTest {
+	
+    @Test public void test() {
+    	assertEquals(true, true);
+    }
+    
+}
