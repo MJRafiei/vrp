6e4a0fb68b941d46f9728d3dab4ca1347aee9bfd
yadegari <yadegari@viratech.ir>
2019-11-03 21:39:18 +0330

    Add bootstrap + Add marker on map click

--- public/app.js
+++ public/app.js
@@ -1,6 +1,17 @@
 angular.module('qaaf', [])
 .controller('Greeting', function($scope, $http) {
     
+	$http.get('/greeting').
+    then(function(response) {
+        $scope.greeting = response.data;
+    });
+	
+	$scope.points = [];
+	$scope.plan = function() {
+		console.log($scope.points);
+		console.log($scope.driverCount);
+	}
+	
     mapboxgl.accessToken = 'pk.eyJ1IjoiYWxpeWRnciIsImEiOiJjazJpcjZjMnExZXRpM3BvMjFqeGwzMWZvIn0.-dW4TEm1efXqzQfEYeu_Iw';
     mapboxgl.setRTLTextPlugin('https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-rtl-text/v0.2.3/mapbox-gl-rtl-text.js');
     var map = new mapboxgl.Map({
@@ -8,6 +19,14 @@ angular.module('qaaf', [])
 	    style: 'mapbox://styles/aliydgr/ck2irkysx262n1cs2appnuoty',
 	    center: [51.384823, 35.742647],
 	    zoom: 13
+    });
+    
+    map.on('click', function (e) {
+    	var marker = new mapboxgl.Marker()
+    	  .setLngLat(e.lngLat)
+    	  .addTo(map);
+    	
+    	$scope.points.push(marker.getLngLat());
     });
     
 });
